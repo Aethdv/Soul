98f351e5f5b5f6e399bdf005703b317a848c9257
Aethdv <aeth@tuta.io>
2026-04-16 03:31:41 +0200

    Getting rid of feature warning in tuner/ Bench: 5141681

--- tuner/src/lib.rs
+++ tuner/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(custom_inner_attributes)]
 // Mathematical implementations heavily rely on standard linear algebra notation (x, y, z, w, m).
 #![allow(clippy::many_single_char_names)]
 // Optimizer states (c_mu, c_sigma, p_c, p_sigma) precisely match CMA-ES literature.
